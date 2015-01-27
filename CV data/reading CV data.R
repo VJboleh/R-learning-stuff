1a64632b45730c5593ca7553476e551596adfd4f
VJboleh <karthikm.kmn@gmail.com>
2015-01-26 17:51:30 -0800

    import Gamry CV data with R

---
+++ CV data/reading CV data.R
@@ -0,0 +1,3 @@
+mydata =  read.table("C:\\Users\\Karthik Murali\\Box Sync\\PhD Research\\Experimental Data\\Gamry\\CV\\012615_KM_baseline2_160mmhg.txt",header = FALSE,skip=137,fill = TRUE)
+mydata[1800:1805,]
+
